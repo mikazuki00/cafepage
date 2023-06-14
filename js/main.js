4c2a9ade107e4f3923891da03373cdd4aa3d0528
mikazuki00 <120359362+mikazuki00@users.noreply.github.com>
2023-06-15 00:03:21 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(function(){
+    $('#hum__btn').on('click',function(){
+        $('#btn__bar,#nav-sp,#header-sp').toggleClass('show');
+    })
+})
+// トップへスクロール
+$(function () {
+    var pagetop = $('#page-top');
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+        pagetop.fadeIn();
+        } else {
+        pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+    });
+
+// クラス付与
+$(window).on('load', function() {
+    var scrollStopEvent = new $.Event('scrollstop');
+        let delay = 200;
+        let timer;
+        function scrollStopEventTrigger(){
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function(){$(window).trigger(scrollStopEvent)}, delay);
+        }
+        $(window).on("scroll", scrollStopEventTrigger);
+        $("body").on("touchmove", scrollStopEventTrigger);
+    var nav = $('.header');
+    var navoffset = nav.offset();
+    $(window).resize(function(){
+        navoffset = nav.offset();
+    });
+   //$(window).scroll(function () {
+    $(window).on("scroll touchmove",function () {
+            if($(window).scrollTop() > navoffset.top) {
+                nav.addClass("fixed");
+            }else{
+                nav.removeClass("fixed");
+            }
+        });
+    });
